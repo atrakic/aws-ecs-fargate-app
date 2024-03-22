ccad37b8dda6f27cf60beb847ed00a51d71d22a1
atrakic <atrakic@users.noreply.github.com>
2024-03-22 12:21:02 +0100

    Update test.sh

--- terraform/tests/test.sh
+++ terraform/tests/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
-curl -k -D- -H 'Host: octocat.example.com' https://$(terraform output -raw alb_hostname)
+curl -k -D- -H 'Host: app.foo.bar' https://$(terraform output -raw alb_hostname)
 aws acm list-certificates --max-items 10
